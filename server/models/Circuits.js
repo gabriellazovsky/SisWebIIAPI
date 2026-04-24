f7109f3e7c1ab39c7bca66b6bb1e65fb08453b35
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2026-04-24 08:06:17 +0200

    circuit model

---
+++ server/models/Circuits.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const CircuitSchema = new mongoose.Schema({
+  circuitId: { type: Number, required: true, unique: true },
+  circuitRef: String,
+  name: String,
+  location: String,
+  country: String,
+  lat: Number,
+  lng: Number,
+  alt: Number,
+  url: String
+});
+
+module.exports = mongoose.model('Circuit', CircuitSchema);
