e71bcf4f0dd200b313081c3e4d90b52ec378c247
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2026-04-24 08:12:41 +0200

    sprint results model

---
+++ server/models/SprintResults.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const SprintResultSchema = new mongoose.Schema({
+  resultId: { type: Number, required: true, unique: true },
+  raceId: Number,
+  driverId: Number,
+  constructorId: Number,
+  number: Number,
+  grid: Number,
+  position: Number,
+  positionText: String,
+  positionOrder: Number,
+  points: Number,
+  laps: Number,
+  time: String,
+  milliseconds: Number,
+  fastestLap: Number,
+  fastestLapTime: String,
+  statusId: Number
+});
+
+module.exports = mongoose.model('SprintResult', SprintResultSchema);
