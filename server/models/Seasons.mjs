72ede7b71a6e8298619a0f35591de21d617dda54
pilarbourg <pilar.bourg@usp.ceu.es>
2026-04-25 16:30:27 +0200

    deleting models to adapt to mongodb

--- server/models/Seasons.mjs
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const SeasonSchema = new mongoose.Schema({
-  year: { type: Number, required: true, unique: true },
-  url: String
-});
-
-module.exports = mongoose.model('Season', SeasonSchema);
--- server/models/Status.mjs
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const StatusSchema = new mongoose.Schema({
-  statusId: { type: Number, required: true, unique: true },
-  status: String
-});
-
-module.exports = mongoose.model('Status', StatusSchema);
