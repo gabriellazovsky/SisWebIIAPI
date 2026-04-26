9d840e37ae2f21aa936c1ae4b50bfff822dac111
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2026-04-26 13:17:23 +0200

    included race circuit and constructor in index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.js";
 import driverRoutes from "./routes/driverRoutes.js";
-//import constructorRoutes from "./routes/constructorRoutes.js"
-//import raceRoutes from "./routes/raceRoutes.js"
-//import circuitRoutes from "./routes/circuitRoutes.js"
+import constructorRoutes from "./routes/constructorRoutes.js"
+import raceRoutes from "./routes/raceRoutes.js"
+import circuitRoutes from "./routes/circuitRoutes.js"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -13,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/drivers", driverRoutes);
-//app.use("/constructors", constructorRoutes);
-//app.use("/races", raceRoutes);
-//app.use("/circuits", circuitRoutes);
+app.use("/constructors", constructorRoutes);
+app.use("/races", raceRoutes);
+app.use("/circuits", circuitRoutes);
 
 app.use((err, _req, res, _next) => {
   res.status(500).send("An unexpected error occured.")
